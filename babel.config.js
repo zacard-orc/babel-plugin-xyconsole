22b63741723f690848e8177028e85923714cc849
zacard.lin <13816025562@139.com>
2022-07-02 10:11:34 +0800

    :art: feat: add babel.config

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+    ],
+    plugins: [
+        ['@babel/plugin-transform-runtime', {
+            absoluteRuntime: false,
+            corejs: false,
+            helpers: true,
+            regenerator: true,
+            useESModules: false,
+        }],
+    ],
+}
